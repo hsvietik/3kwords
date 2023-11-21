bf90b3284ff77ebe87f1126786b5345db4410bfc
hsvietik <hsvietik@users.noreply.github.com>
2023-11-21 23:18:12 +0000

    deploy: 36d1f1c1ae33a3a84a20c7a75bb330ade9f13821

--- assets/Settings-b15ad388.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-15eb1169.js";const r=()=>t.jsx("div",{});export{r as default};
--- assets/Study-77c2beb8.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-15eb1169.js";const s=()=>t.jsx("div",{});export{s as default};
