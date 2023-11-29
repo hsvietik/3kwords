d6f55ad32b90d6b2c16a9209b636c9673e93a260
Hanna Svietik <hsvietik@gmail.com>
2023-11-29 23:07:04 +0000

    add: loader gradient background

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,8 +1,30 @@
 import styled from "styled-components";
 
 export const AnimatedComponent = styled.div`
-  background: var(--color-pink);
-  background: var(--background-gradient);
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+  height: 100vh;
+  width: 100vw;
+  /* background: var(--color-pink);
+  background: var(--background-gradient); */
+  background: linear-gradient(
+    -45deg,
+    var(--color-purple),
+    var(--color-pink),
+    var(--color-blue),
+    var(--color-turquoise)
+  );
+  animation: gradient 5s ease infinite;
+  background-size: 200% 200%;
   padding: 20px;
   border-radius: 6px;
   font-weight: 800;
