1a6bcf3a0a526c3af39a4c2aa82c6d88a4730596
Hanna Svietik <hsvietik@gmail.com>
2023-12-08 23:27:51 +0000

    add: carousel in hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,14 @@
-import heroPicture from "../../assets/words-have-power.jpg";
+// import heroPicture from "../../assets/words-have-power.jpg";
+import { carouselData } from "../../data/carousel";
+import { Carousel, Slide } from "./Hero.styled";
+import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
+
 export const Hero = () => {
   return (
-    <div>
-      <img src={heroPicture} alt="Words have power" />
-    </div>
+    <Carousel>
+      {carouselData.map(({ src, alt }, idx) => {
+        return <Slide src={src} alt={alt} key={idx} />;
+      })}
+    </Carousel>
   );
 };
---
+++ src/data/carousel.js
@@ -0,0 +1,22 @@
+import carouselFirst from "../assets/words-have-power.jpg";
+import carouselSecond from "../assets/word.jpg";
+import carouselThird from "../assets/words-have-power-left-letters.jpg";
+import carouselFourth from "../assets/word-left-letters.jpg";
+export const carouselData = [
+  {
+    src: carouselFirst,
+    alt: "Letters 'Words have power'",
+  },
+  {
+    src: carouselSecond,
+    alt: "Letters 'Word'",
+  },
+  {
+    src: carouselThird,
+    alt: "Letters 'Words have power'",
+  },
+  {
+    src: carouselFourth,
+    alt: "Letters 'Word'",
+  },
+];
